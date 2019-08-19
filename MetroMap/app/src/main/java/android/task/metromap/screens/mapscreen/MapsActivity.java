cceffa8100221029472ae554ce55b4ff7f52b066
RadyaAlbasha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-08-19 18:26:20 +0200

    Update MapsActivity.java

--- MetroMap/app/src/main/java/android/task/metromap/screens/mapscreen/MapsActivity.java
+++ MetroMap/app/src/main/java/android/task/metromap/screens/mapscreen/MapsActivity.java
@@ -3,30 +3,9 @@ package android.task.metromap.screens.mapscreen;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.task.metromap.R;
-import android.task.metromap.model.Metro;
-import android.task.metromap.model.services.JsonResponse;
-import android.task.metromap.model.services.RetrofitInterface;
-import android.widget.Toast;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import okhttp3.Cache;
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , MapContract.IView {
 
@@ -54,7 +33,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
